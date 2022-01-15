796c8fa358a73620bb57adbf2bfe3a09ea32c58e
Elwardi <elwardifadeli@gmail.com>
2022-01-15 18:32:20 +0100

    fix(graph): better access specifiers

--- cpp_graph.hpp
+++ cpp_graph.hpp
@@ -7,19 +7,20 @@ namespace Foam
         public scoped::templateParent<1, double>,
         public justTemplateParent<1, double>
     {
+		Variable& set_;
+		Variable const&& lval_;
+		const Variable && cntlval_;
     public:
 		static Variable st_;
         Member public_m;
 		virtual void method(const double&, const int& a) = 0;
-		static void anotherMethod(const double&, const int& a);
-		template<int N>
-		Member<N> public_t;
-		template<int N>
-		static Cool anotherOne;
-        template<class T>
-		void templateMethod(const T&, const int& a);
-        someClass(int n, Vector v);
+		static virtual void anotherMethod(const double&, const int& a) = 0;
+		template<int N> Member<N> public_t;
+		template<int N> static Cool& anotherOne;
     protected:
+        template<class T>
+		void templateMethod(const T&, const int& a) const;
+        someClass(int n, Vector v);
         SomeVar v_;
         AnotherVar<double> tv_;
     };
